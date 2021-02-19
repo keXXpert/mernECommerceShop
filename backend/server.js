f2579d4b1ec9e180677c9238a6c4827013fa9040
keXXpert <kexxpert@ya.ru>
2021-02-19 22:55:57 +0300

    added error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { errorHandler, errorNotFound } from './middleware/errorMiddleware.js'
 
 // routes
 import productRoutes from './routes/productRoutes.js'
@@ -17,6 +18,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 
+app.use(errorNotFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
